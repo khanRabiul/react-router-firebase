76494425f4c2465c852107b3e0ade83ff97feb61
khan-rabiul <krabiulislam2@gmail.com>
2024-11-24 18:33:11 +0600

    firebase auth functionality done remain to impelement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Router, Routes, Route } from "react-router-dom";
 import Layout from "./pages/layout/Layout";
+import SignUp from "./pages/Signup";
+import Users from "./pages/Users";
+import SignIn from "./pages/Signin";
 
 function App() {
   return (
     <>
-      <Layout />
+      <Router>
+        <Layout />
+        <Routes>
+          <Route path="/signup" element={<SignUp /> } />
+          <Route path="/signin" element={<SignIn /> } />
+          <Route path="/users" element={<Users /> } />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-catch */
-import { createUserWithEmailAndPassword, sendEmailVerification, signOut, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { auth } from "./firebase.config";
+import { FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth/web-extension";
 
 const registerWithEmailAndPassword = async (name, email, password) => {
 try{
@@ -16,4 +17,62 @@ await signOut(auth); // ইউজারকে লগআউট করা
 }
 };
 
-export {registerWithEmailAndPassword}
+// Login with email and password
+const loginWithEmailAndPassword = async (email, password) => {
+    try {
+        const response = await signInWithEmailAndPassword (auth, email, password);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Login with google
+const loginWithGoogle = async () => {
+    try {
+        const response = await signInWithPopup (auth, GoogleAuthProvider);
+        return response.user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Login with github
+const loginWithGithub = async () => {
+    try{
+        const response = await signInWithPopup(auth, GithubAuthProvider);
+        return response.user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Login with Facebook
+const loginWihtFacebook = async () => {
+    try {
+        const res = await signInWithPopup (auth, FacebookAuthProvider);
+        const user = res.user;
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Reset Password
+const sendRestPassword = async (email) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+    } catch (error) {
+        throw error;
+    }
+};
+
+// Export all functions
+export {
+    registerWithEmailAndPassword,
+    loginWithEmailAndPassword,
+    loginWithGoogle,
+    loginWithGithub,
+    loginWihtFacebook,
+    sendPasswordResetEmail
+}
